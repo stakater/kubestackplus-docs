9f1e512aeea6dc4f25781d6fcc3da1b299c3066d
Zoha Jamil <zoha@aurorasolutions.io>
2024-07-15 19:39:22 +0400

    adjusted test as it doesn't run on full screen

--- testcafe/src/run.ts
+++ testcafe/src/run.ts
@@ -4,6 +4,7 @@ const consentControlsDiv = Selector('.md-consent__controls');
 const acceptButton = consentControlsDiv.find('button').withText('Accept');
 const searchInput = Selector('input.md-search__input').withAttribute('placeholder', 'Search');
 const searchResultItem = Selector('li.md-search-result__item');
+const searchLabel = Selector('label.md-header__button.md-icon').withAttribute('for', '__search');
 
 
 fixture("Check if yamls are valid")
@@ -17,7 +18,14 @@ test(`Accept cookies if cookies consent message is displayed`, async (t) => {
         await t.click(acceptButton);
     }
 
-    await t.expect(searchInput.visible).ok({ timeout: 5000 }); // Wait up to 5 seconds
+    // Wait for 5 seconds for search label to become visible
+    await t.expect(searchLabel.visible).ok({ timeout: 5000 });
+    const searchLabelExists = await searchLabel.exists;
+    if (searchLabelExists) {
+        await t.click(searchLabel);
+    }
+
+    await t.expect(searchInput.visible).ok(); // Wait up to 5 seconds
     await t.click(searchInput);
     await t.typeText(searchInput, "```")
     console.log('Search input clicked and typed on');
